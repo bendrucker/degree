1a2a17da41f848462e35fdfdf6347360879a4831
Ben Drucker <bvdrucker@gmail.com>
2015-08-05 11:51:41 -0700

    Use require.resolve to turn partial filenames or '.' into full names

--- plugin.js
+++ plugin.js
@@ -1,12 +1,11 @@
 'use strict'
 
-var path = require('path')
 var createTransform = require('./transform')
 
 module.exports = function degreePlugin (browserify, options) {
   browserify.transform(createTransform(options.files.map(resolve)))
 
   function resolve (file) {
-    return path.resolve(browserify.basedir || process.cwd(), file)
+    return require.resolve(browserify.basedir || process.cwd(), file)
   }
 }
