954de225ac254357e45073d04d0fff31e96880eb
Ben Drucker <bvdrucker@gmail.com>
2015-08-05 12:00:02 -0700

    Fix resolving with node-resolve
    Fn signature is not the same as path.resolve, need custom implementation for basedir

--- plugin.js
+++ plugin.js
@@ -1,11 +1,14 @@
 'use strict'
 
+var resolveSync = require('resolve').sync
 var createTransform = require('./transform')
 
 module.exports = function degreePlugin (browserify, options) {
   browserify.transform(createTransform(options.files.map(resolve)))
 
   function resolve (file) {
-    return require.resolve(browserify.basedir || process.cwd(), file)
+    return resolveSync(file, {
+      basedir: browserify.basedir || process.cwd()
+    })
   }
 }
