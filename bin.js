0072cefe81c0e39d5887943c20a1dc0f58a844b7
Ben Drucker <bvdrucker@gmail.com>
2016-02-05 09:12:54 -0800

    Remove server log statement (handled by budo)

--- bin.js
+++ bin.js
@@ -18,8 +18,3 @@ var entries = cli.input
 if (!entries.length) throw new Error('entries are required')
 
 degree(entries, extend(cli.flags, {stream: process.stdout}))
-  .on('connect', function (server) {
-    var uri = server.uri
-    console.log('Server running at', uri)
-    open(uri)
-  })
