7eab25867e69e2d10437520daa4605a709865558
Ben Drucker <bvdrucker@gmail.com>
2016-02-05 08:48:09 -0800

    Log to stdout

--- bin.js
+++ bin.js
@@ -4,6 +4,7 @@
 
 var open = require('opn')
 var meow = require('meow')
+var extend = require('xtend')
 var degree = require('./')
 
 var cli = meow({
@@ -16,7 +17,7 @@ var cli = meow({
 var entries = cli.input
 if (!entries.length) throw new Error('entries are required')
 
-degree(entries, cli.flags)
+degree(entries, extend(cli.flags, {stream: process.stdout}))
   .on('connect', function (server) {
     var uri = server.uri
     console.log('Server running at', uri)
